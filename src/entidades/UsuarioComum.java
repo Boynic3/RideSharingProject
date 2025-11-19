3706926b9b75ccc247a3c57c60e5cccab280faa5
Cliente <Cliente@PCDOARTHUR>
2025-11-18 22:17:06 -0300

    Adicionado método construtor "nome"

--- src/entidades/UsuarioComum.java
+++ src/entidades/UsuarioComum.java
@@ -1,9 +1,14 @@
 package entidades;
 
+
 public class UsuarioComum {
     private String nome,
     cpf,
     email,
     senha,
     telefone;
+    
+    public UsuarioComum(String nome) {
+    	this.nome = nome;
+    }
 }
