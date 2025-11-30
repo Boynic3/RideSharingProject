59a23f2e8addd458d0013794a289fcc007f1c09f
Matheus Moretti Soares <matheusmorettisoares@gmail.com>
2025-11-29 21:42:21 -0300

    Comentário em MetodoPagamento e sincronizações no UML

--- src/entidades/MetodoPagamento.java
+++ src/entidades/MetodoPagamento.java
@@ -1,7 +1,7 @@
 package entidades;
 
 public interface MetodoPagamento {
-    static boolean validaPagamento(Passageiro passageiro) {
+    static boolean validaPagamento(Passageiro passageiro) { //como é static, não precisa implementar em cada classe que implementa a interface
         return true;
     }
 
