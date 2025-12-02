38277f5b9141a78b10a1b3e445bcb6eb2b31fade
Boynic3 <matheusmorettisoares@gmail.com>
2025-12-01 22:26:04 -0300

    Atualizações no Menu Principal

--- src/ui/Principal.java
+++ src/ui/Principal.java
@@ -6,7 +6,7 @@ public class Principal {
         Scanner ler = new Scanner(System.in);
         int opcao = 1;
 
-        System.out.println("Ride Sharing Iniciado");
+        System.out.println(" ==== Ride Sharing Iniciado ====");
         do {
             System.out.println("1- Cadastrar Passageiro");
             System.out.println("2- Cadastrar Motorista");
@@ -38,28 +38,40 @@ public class Principal {
                         cadastrarMotorista();
                         break;
                     case 3:
+                        cadastrarVeiculo();
                         break;
                     case 4:
+                        alterarStatus();
                         break;
                     case 5:
+                        solicitarCorrida();
                         break;
                     case 6:
+                        iniciarViagem();
                         break;
                     case 7:
+                        finalizarViagem();
                         break;
                     case 8:
+                        cancelarViagem();
                         break;
                     case 9:
+                        processarPagamento();
                         break;
                     case 10:
+                        verCorridas();
                         break;
                     case 11:
+                        listarMotoristas();
                         break;
                     case 12:
+                        listarPassageiros();
                         break;
                     case 13:
+                        avaliarMotorista();
                         break;
                     case 14:
+                        avaliarPassageiro();
                         break;
                     default:
                         System.out.println("Opção INVÁLIDA. Tente novamente.");
@@ -78,10 +90,90 @@ public class Principal {
     }
 
     private static void cadastrarMotorista() {
+
+        /// ///
         System.out.println("Cadastro de motorista realizado."); // Colocar codigo aq dps
     }
 
     private static void cadastrarPassageiro() {
+
+        /// ///
         System.out.println("Cadastro de passageiro realizado."); //Mudar isso dps
     }
+
+    private static void cadastrarVeiculo() {
+
+        /// ///
+        System.out.println("Cadastro de veículo realizado.");
+    }
+
+    private static void alterarStatus() {
+
+        /// ///
+        System.out.println("Status alterado");
+    }
+
+    private static void solicitarCorrida() {
+
+        /// ///
+        System.out.println("Corrida solicitada");
+    }
+
+    private static void iniciarViagem() {
+
+        /// ///
+        System.out.println("Viagem iniciada");
+    }
+
+    private static void finalizarViagem() {
+
+        /// ///
+        System.out.println("Viagem finalizada");
+    }
+
+    private static void cancelarViagem() {
+
+        /// ///
+        System.out.println("Viagem cancelada");
+    }
+
+    private static void processarPagamento() {
+
+        /// ///
+        System.out.println("Pagamento processado");
+    }
+
+    private static void verCorridas() {
+        System.out.println("Corridas: ");
+
+
+    }
+
+    private static void listarMotoristas() {
+        System.out.println("Motoristas: ");
+
+
+    }
+
+    private static void listarPassageiros() {
+        System.out.println("Passageiros: ");
+
+
+    }
+
+    private static void avaliarMotorista() {
+        System.out.println("==Avaliar motoristas==");
+        System.out.println("Qual motorista você quer avaliar: ");
+
+
+    }
+
+    private static void avaliarPassageiro() {
+        System.out.println("==Avaliar passageiros==");
+        System.out.println("Qual passageiro você quer avaliar: ");
+
+
+    }
+
+
 }
